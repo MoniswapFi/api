57ca0d9203e359df4645a08c520c0346381416bf
Kingsley Victor <javaprodigy56@gmail.com>
2024-08-08 07:27:55 +0100

    made updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const port = parseInt(process.env.PORT || "1998");
+const router = require("./router");
 
 const app = express();
 
+app.use(router);
 app.listen(port, () => console.info("Server running on %d", port));
